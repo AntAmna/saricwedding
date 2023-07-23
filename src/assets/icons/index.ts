22a88ac841b98104f07a57add9bcfb6edab441cc
AntAmna <amna.subic@antcolony.io>
2023-07-23 18:26:18 +0200

    adding icons, linter

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,15 @@
+export { default as Doves } from './Doves';
+export { default as WeddingRings } from './WeddingRings';
+export { default as WeddingInvitation } from './WeddingInvitation';
+export { default as Bouquet } from './Bouquet';
+export { default as LoveDoves } from './LoveDoves';
+export { default as BrideInArms } from './BrideInArms';
+export { default as Cake } from './Cake';
+export { default as Camera } from './Camera';
+export { default as FirstDance } from './FirstDance';
+export { default as Chairs } from './Chairs';
+export { default as Music } from './Music';
+export { default as Bride } from './Bride';
+export { default as Groom } from './Groom';
+export { default as FlowerGirl } from './FlowerGirl';
+export { default as RingBearer } from './RingBearer';
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,10 @@ import App from './components/App';
 
 import './styles/index.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
